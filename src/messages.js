07e7323de0e3feae18cac19a17728c129284dad2
be5invis <belleve@typeof.net>
2016-07-16 07:13:43 +0800

    update messages

--- src/messages.js
+++ src/messages.js
@@ -1,10 +1,10 @@
 exports.messages = {
 	admin: 'Run VS Code with admin privileges so the changes can be applied.',
-	enabled: 'Custom CSS enabled. Restart to take effect.',
-	disabled: 'Custom CSS disabled and reverted to default CSS. Restart to take effect.',
-	already_disabled: 'Custom CSS already disabled.',
+	enabled: 'Custom CSS and JS enabled. Restart to take effect.',
+	disabled: 'Custom CSS and JS disabled and reverted to default. Restart to take effect.',
+	already_disabled: 'Custom CSS and JS already disabled.',
 	smthingwrong: 'Something went wrong: ',
 	restartIde: 'Restart Visual Studio Code',
-	notfound: 'Custom CSS not found.',
-	notconfigured: 'Custom CSS path not configured. Please set "vscode_custom_css.imports" in your user settings.'
+	notfound: 'Custom CSS and JS not found.',
+	notconfigured: 'Custom CSS and JS path not configured. Please set "vscode_custom_css.imports" in your user settings.'
 };
