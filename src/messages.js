7111fdf764144a42be21f3609f85479216f2c3e0
be5invis <belleve@typeof.net>
2017-03-19 18:17:24 +0800

    fix typo

--- src/messages.js
+++ src/messages.js
@@ -1,6 +1,6 @@
 exports.messages = {
 	admin: 'Run VS Code with admin privileges so the changes can be applied.',
-	enabled: 'Custom CSS and JS enabled. Restart to take effect. If Code complains about it is corrupted, CLICK DON'T SHOW AGAIN. See README for more detail.',
+	enabled: 'Custom CSS and JS enabled. Restart to take effect. If Code complains about it is corrupted, CLICK DON\'T SHOW AGAIN. See README for more detail.',
 	disabled: 'Custom CSS and JS disabled and reverted to default. Restart to take effect.',
 	already_disabled: 'Custom CSS and JS already disabled.',
 	smthingwrong: 'Something went wrong: ',
